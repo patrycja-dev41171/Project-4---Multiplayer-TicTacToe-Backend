65879410db71980dbd67fdd31934d59d0f6599e5
Patrycja Ińska <patrycja.inska@interia.pl>
2022-09-09 19:54:25 +0200

    A configuration file and a file with the server connection to the database have been created.

--- index.ts
+++ index.ts
@@ -1,20 +1,32 @@
-import * as express from "express";
 import "express-async-errors";
+import * as dotenv from 'dotenv';
 import { appRouter } from "./routers/app";
-import * as cors from "cors";
+import rateLimit from "express-rate-limit";
+import express from "express";
+import cors from "cors";
 
 const app = express();
+
+dotenv.config({ path: ".env" });
 
 app.use(
   cors({
     origin: "http://localhost:3000",
+    credentials: true,
   })
 );
 
-app.use(express.json()); // Content-type: application/json
+app.use(express.json());
+app.use(
+  rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 500,
+    standardHeaders: true,
+    legacyHeaders: false,
+  })
+);
 
 app.use("/", appRouter);
-
 
 app.listen(3001, "0.0.0.0", () => {
   console.log("Listening on http://localhost:3001");
