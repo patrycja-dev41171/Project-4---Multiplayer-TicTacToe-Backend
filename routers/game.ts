c0009174d13aa6b9f07ddae3359a1101a2efa665
Patrycja Ińska <patrycja.inska@interia.pl>
2022-09-17 00:50:10 +0200

    Create three endpoints for gameRouter to start or join a player to a game.

--- routers/game.ts
+++ routers/game.ts
@@ -1,21 +1,76 @@
 import { Router } from "express";
 import { RoomRecord } from "../records/room.record";
 import { ValidationError } from "../utils/handleErrors";
+import { Room } from "../types";
 
 export const gameRouter = Router();
 
-gameRouter.get("/start/:user_id", async (req, res) => {
-  console.log(req.params.user_id.length);
+gameRouter
 
-  try {
-    const room = await new RoomRecord({
-      first_user_id: req.params.user_id,
-    });
-    const room_id = await room.insert();
-    res.json({
-      room_id: room_id,
-    });
-  } catch (err) {
-    throw new ValidationError("An error occurred while starting the game.");
-  }
-});
+  .get("/start/friend/:user_id", async (req, res) => {
+    try {
+      const room = await new RoomRecord({
+        first_user_id: req.params.user_id,
+        room_type: Room.friend,
+      });
+      const room_id = await room.insert();
+      res.json({
+        room_id: room_id,
+      });
+    } catch (err) {
+      throw new ValidationError("An error occurred while starting the game.");
+    }
+  })
+
+  .post("/join/friend/:user_id", async (req, res) => {
+    const room = await RoomRecord.getOneByRoom_id(req.body.code);
+
+    if (room === null) {
+      throw new ValidationError("Invalid code.");
+    }
+
+    if (room.second_user_id !== null) {
+      throw new ValidationError("Too many game participants.");
+    }
+
+    try {
+      await RoomRecord.joinRoom(req.params.user_id);
+      res.json({
+        room_id: room.room_id,
+      });
+    } catch (err) {
+      throw new ValidationError("An error occurred while joining the game.");
+    }
+  })
+
+  .get("/start/random/:user_id", async (req, res) => {
+    await RoomRecord.deleteRoomByUser(req.params.user_id);
+
+    const room = await RoomRecord.getFreeRandomRoom();
+
+    if (room === null) {
+      try {
+        const room = await new RoomRecord({
+          first_user_id: req.params.user_id,
+          room_type: Room.random,
+        });
+        const room_id = await room.insert();
+        res.json({
+          room_id: room_id,
+          startGame: false,
+        });
+      } catch (err) {
+        throw new ValidationError("An error occurred while starting the game.");
+      }
+    } else {
+      try {
+        await RoomRecord.joinRoom(req.params.user_id);
+        res.json({
+          room_id: room.room_id,
+          startGame: true,
+        });
+      } catch (err) {
+        throw new ValidationError("An error occurred while joining the game.");
+      }
+    }
+  });
