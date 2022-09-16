0e21f1a48abb188bd3074ec73780ac54f51fe666
Patrycja Ińska <patrycja.inska@interia.pl>
2022-09-17 00:48:13 +0200

    Adding a new field in the Room type, changes in the RoomRecord class. Adding methods: getOneByRoom_id (roo_id); deleteRoomByUser (first_user_id); joinRoom (second_user_id); getFreeRandomRoom (); deleteRoom (room_id).
    Fixing errors in the UserRecord class in the getOneByUserId (user_id) method;

--- records/room.record.ts
+++ records/room.record.ts
@@ -1,12 +1,16 @@
 import { ValidationError } from "../utils/handleErrors";
 import { pool } from "../utils/db/db";
 import { v4 as uuid } from "uuid";
-import { RoomEntity } from "../types";
+import { RoomEntity, Room } from "../types";
+import { FieldPacket } from "mysql2";
+
+type RoomResults = [RoomEntity[], FieldPacket[]];
 
 export class RoomRecord implements RoomEntity {
   room_id?: string;
   first_user_id: string;
   second_user_id?: string | null;
+  room_type: Room;
 
   constructor(obj: RoomEntity) {
     if (!obj.first_user_id || obj.first_user_id.length !== 36) {
@@ -19,14 +23,61 @@ export class RoomRecord implements RoomEntity {
     this.room_id = obj.room_id ?? uuid();
     this.first_user_id = obj.first_user_id;
     this.second_user_id = obj.second_user_id ?? null;
+    this.room_type = obj.room_type;
   }
 
   async insert(): Promise<string> {
-    console.log(this)
     await pool.execute(
-      "INSERT INTO `room`(`room_id`,`first_user_id`)VALUES(:room_id,:first_user_id)",
+      "INSERT INTO `room`(`room_id`,`first_user_id`," +
+        " `room_type`)VALUES(:room_id,:first_user_id,:room_type)",
       this
     );
     return this.room_id;
+  }
+
+  static async getOneByRoom_id(room_id: string): Promise<RoomEntity> {
+    const [results] = (await pool.execute(
+      "SELECT * FROM `room` WHERE `room_id` =" + " :room_id",
+      {
+        room_id,
+      }
+    )) as RoomResults;
+    return results.length === 0 ? null : results[0];
+  }
+
+  static async deleteRoomByUser(first_user_id: string): Promise<void> {
+    await pool.execute(
+      "DELETE FROM `room` WHERE `first_user_id` = :first_user_id",
+      {
+        first_user_id: first_user_id,
+      }
+    );
+  }
+
+  static async joinRoom(second_user_id: string): Promise<void> {
+    await pool.execute("UPDATE `room` SET `second_user_id` = :second_user_id", {
+      ...this,
+      second_user_id: second_user_id,
+    });
+  }
+
+  static async getFreeRandomRoom(): Promise<RoomEntity> {
+    const [results] = (await pool.execute(
+      "SELECT * FROM `room` WHERE `room_type` = :room_type" +
+        " AND  `second_user_id` IS NULL",
+      {
+        room_type: Room.random,
+      }
+    )) as RoomResults;
+    return results.length === 0 ? null : results[0];
+  }
+
+  static async deleteRoom(room_id: string): Promise<void> {
+    await pool.execute(
+      "DELETE FROM `room` WHERE `room_id` = :room_id AND  `second_user_id` IS NULL",
+      {
+        room_id: room_id,
+      }
+    );
   }
 }
--- types/room/room.ts
+++ types/room/room.ts
@@ -1,5 +1,11 @@
+export enum Room {
+  friend = "friend",
+  random = "random",
+}
+
 export type RoomEntity = {
   room_id?: string;
   first_user_id: string;
   second_user_id?: string | null;
+  room_type: Room;
 };
