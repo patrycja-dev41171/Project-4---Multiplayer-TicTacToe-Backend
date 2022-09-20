80fc5bec1c0d0f8d1adb6bd30607bc1baeb90d4a
Patrycja Ińska <patrycja.inska@interia.pl>
2022-09-20 17:52:01 +0200

    Pre deploy changes.

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express, { Router } from "express";
+import express from "express";
 import "express-async-errors";
 const http = require("http");
 import cookieParser from "cookie-parser";
@@ -6,22 +6,22 @@ import * as dotenv from "dotenv";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
 import cors from "cors";
-import { Server } from "socket.io";
+import {Server, Socket} from "socket.io";
 import { auth } from "./utils/auth";
 import { handleError } from "./utils/handleErrors";
-
 import { homeDataRouter } from "./routers/homeData";
 import { gameRouter } from "./routers/game";
 import { refreshTokenRouter } from "./routers/refreshToken";
 import { signUpRouter } from "./routers/signUp";
 import { loginRouter } from "./routers/login";
 import { config } from "./config/config";
+import {GameMove, GameResults} from "./types";
 
 const app = express();
 
 const server = http.createServer(app);
 
-export const io = new Server(server, {
+const io = new Server(server, {
   cors: {
     origin: config.corsOrigin,
     methods: ["GET", "POST"],
@@ -57,6 +57,40 @@ app.use("/api/home", auth, homeDataRouter);
 
 app.use(handleError);
 
+
+io.on("connection", (socket: Socket) => {
+    socket.on("join_room", (room_id:string) => {
+        socket.join(room_id);
+        socket.to(room_id).emit("player_join");
+    });
+
+    socket.on("game-move", (data: GameMove) => {
+        socket.to(data.room).emit("other_user_move", data);
+    });
+
+    socket.on("game-results", (data: GameResults) => {
+        socket.to(data.room).emit("game-results", data);
+    });
+
+    socket.on("disconnecting", function () {
+        const id = socket.id;
+        socket.rooms.forEach(function (room:string) {
+            if (room != id) {
+                socket.to(room).emit("user-disconnect", { room: room });
+            }
+        });
+    });
+
+    socket.on("user-disconnect", function () {
+        const id = socket.id;
+        socket.rooms.forEach(function (room: string) {
+            if (room != id) {
+                socket.to(room).emit("user-disconnect", { room: room });
+            }
+        });
+    });
+});
+
 server.listen(3001, "0.0.0.0", () => {
-  console.log("Listening on http://localhost:3001");
+    console.log("Listening on http://localhost:3001");
 });
